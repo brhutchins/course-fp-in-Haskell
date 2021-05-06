300d07e51f23e0f79cf2fe2067a3cfcb2ac617d7
Barnaby Hutchins <bhutchins@gmail.com>
2021-05-06 23:16:11 +0200

    first commit

---
+++ Week 3/speller.hs
@@ -0,0 +1,15 @@
+speller :: [String] -> String
+speller xss = foldr (addComma . xIsFor) "" $ removeNullWordsFrom xss
+    where addComma xs ys
+            | null ys   = "and " ++ xs
+            | otherwise = xs ++ ", " ++ ys
+
+          xIsFor word
+            | null word = []
+            | otherwise = [head word] ++ " is for " ++ word
+          
+          removeNullWordsFrom xss =
+            filter (not . null) xss
+          -- If the initial list of words has some null entries in it,
+          -- the output will have extra "and"s sprinkled throughout.
+          -- removeNullWordsFrom fixes that by cleaning up the input.
